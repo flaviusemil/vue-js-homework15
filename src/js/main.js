0ac8c3fa2330a3ddf9892fdafd88c10833c6547d
Flavius Condurache, INI-DEV-COR-BSD (EXT) <flavius.condurache@swisscom.com>
2018-03-20 19:19:50 +0200

    first exercise

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,15 @@ const $ = require('jquery');
 global.$ = global.jQuery = $;
 
 require('bootstrap');
+
+let app = new Vue({
+    el: '#homework-15',
+    data: {
+        value: 'test'
+    },
+    methods: {
+        sayHi: function () {
+            alert("Sample alert");
+        }
+    }
+});
